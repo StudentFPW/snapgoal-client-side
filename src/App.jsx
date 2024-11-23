eadd28b34d281fb150c07ddad2cb745a74a8ac04
Veronika <xafling@mail.ru>
2024-11-23 12:29:51 +0200

    first commit with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl font-bold text-blue-600">
+        Click on the Vite and React logos to learn more
+      </div>
+    </>
+  )
+}
+
+export default App
